ca6da39c02efa8dc96b54c58ee60204befc9afb1
RomanMelnikov77 <79645773155@yandex.ru>
2022-08-05 01:17:32 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -0,0 +1,21 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(long id);
+
+    List<User> getAllUsers();
+
+    User getUserByLogin(String login);
+
+    User getUserById(long id);
+}
